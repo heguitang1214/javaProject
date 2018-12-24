701d5becab8faaf0fc7254240e63c9dde8c40c39
ex-heguitang <ex-heguitang@banketech.com>
2018-12-24 14:12:20 +0800

    mq削峰

---
+++ mq-peakClipping/ticket_api/src/main/java/ai/yunxi/service/impl/TicketServiceImpl.java
@@ -0,0 +1,45 @@
+package ai.yunxi.service.impl;
+
+import ai.yunxi.utils.SnowflakeOrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ai.yunxi.entity.Ticket;
+import ai.yunxi.mapper.ITicketMapper;
+import ai.yunxi.service.ITicketService;
+import ai.yunxi.utils.JsonUtil;
+
+import javax.annotation.Resource;
+
+/**
+ * 
+ * @author 小五老师-云析学院
+ * @createTime 2018年10月24日 下午9:36:17
+ * 
+ */
+@Service
+public class TicketServiceImpl implements ITicketService {
+	
+	@Resource
+	private ITicketMapper ticketMapper;
+
+	@Resource
+	private SnowflakeOrderServiceImpl idGeneration;
+
+	@Override
+	public String createTicket(String userId) {
+		try {
+			Ticket ticket = new Ticket(String.valueOf(idGeneration.nextId()), userId);
+			System.out.println("insert into ticket:ticketId="+ticket.getTicketId());
+			int row = ticketMapper.createTicket(ticket);
+			if(row>0){
+				return JsonUtil.fromBean(ticket);
+			}
+			System.out.println("--------------------null");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
---
+++ mq-peakClipping/ticket_api/src/main/java/ai/yunxi/utils/MQBusiness.java
@@ -0,0 +1,31 @@
+package ai.yunxi.utils;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import ai.yunxi.service.ITicketService;
+
+/**
+ * 
+ * @author 小五老师-云析学院
+ * @createTime 2018年10月26日 下午4:36:38
+ * 
+ */
+@Component
+@RabbitListener(queues=MQProperties.QUEUE_NAME)
+public class MQBusiness {
+
+	@Autowired
+	private ITicketService ticketService;
+	@Autowired
+	private AmqpTemplate amqpTemplate;
+	
+	@RabbitHandler
+	public void process(String userId){
+		String result = ticketService.createTicket(userId);
+		amqpTemplate.convertAndSend(MQProperties.EXCHANGE_NAME, MQProperties.ROUTE_KEY_RESP, result);
+	}
+}
