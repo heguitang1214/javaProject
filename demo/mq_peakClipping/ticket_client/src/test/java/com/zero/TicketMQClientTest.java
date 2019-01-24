cc86a8da21f0e3745cac3a3a08c62e4c6947268e
ex-heguitang <ex-heguitang@banketech.com>
2019-01-24 11:45:31 +0800

    项目结构调整

---
+++ demo/mq_peakClipping/ticket_client/src/test/java/com/zero/TicketMQClientTest.java
@@ -0,0 +1,52 @@
+package com.zero;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import com.zero.utils.MQProperties;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes=TicketClientApplication.class)
+public class TicketMQClientTest {
+
+
+	@Autowired
+	private AmqpTemplate amqpTemplate;
+	
+	private final int THREAD_NUM=1000;
+	private final CountDownLatch cdl = new CountDownLatch(THREAD_NUM);
+	
+	public void test0(){
+		amqpTemplate.convertAndSend(MQProperties.EXCHANGE_NAME, MQProperties.ROUTE_KEY, "100101");
+	}
+	
+	@Test
+	public void test1() {
+		for (int i = 0; i < THREAD_NUM; i++) {
+			new Thread(new Runnable() {
+//				@Override
+				public void run() {
+					try {
+						cdl.await();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					amqpTemplate.convertAndSend(MQProperties.EXCHANGE_NAME, MQProperties.ROUTE_KEY, "100101");
+				}
+			}).start();
+			cdl.countDown();
+		}
+		try {
+			TimeUnit.SECONDS.sleep(30);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
