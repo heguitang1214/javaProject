b4a6ed7df01a590d0457fd2addd6376153a1c1b6
ex-heguitang <ex-heguitang@banketech.com>
2019-01-24 14:57:37 +0800

    springBoot

--- demo/springBoot/src/main/java/com/zero/Application.java
+++ demo/springBoot/src/main/java/com/zero/Application.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//指定扫描的包，springBooboot默认的扫描包为：com.example.demo.xxx
+//默认所扫描Application.class所在的包及其子包，与SpringApplication.run()中的参数有关
+//@ComponentScan("controller")
+//@ComponentScan(basePackages = {"controller", "spring.aop"})//"spring.aopAnnotation.aop",
 public class Application {
 
 	public static void main(String[] args) {
