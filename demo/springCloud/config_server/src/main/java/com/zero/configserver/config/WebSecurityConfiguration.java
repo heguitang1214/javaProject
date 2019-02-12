a7f29ea12884eb4d2d2374738b27fe3766c2025b
ex-heguitang <ex-heguitang@banketech.com>
2019-02-12 18:02:03 +0800

    Spring Cloud

--- demo/springCloud/config_server/src/main/java/com/zero/configserver/config/WebSecurityConfiguration.java
+++ demo/springCloud/config_server/src/main/java/com/zero/configserver/config/WebSecurityConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 /**
- *
+ * 接口安全
  */
 @Configuration
 @EnableWebSecurity
