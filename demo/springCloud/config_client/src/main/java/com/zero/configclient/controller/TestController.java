452ff6f2d27dd24cd1d8b712ec1a105cb860cf40
ex-heguitang <ex-heguitang@banketech.com>
2019-02-12 18:01:14 +0800

    Spring Cloud

--- demo/springCloud/config_client/src/main/java/com/zero/configclient/controller/TestController.java
+++ demo/springCloud/config_client/src/main/java/com/zero/configclient/controller/TestController.java
@@ -7,14 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * @ClassName TestController
- * @Description TODO
- * @Author 云析-路飞
- * @Date 2018/5/9 16:12
- * @Version 1.0
+ *  测试获取spring cloud配置文件
  */
 @Controller
-@RefreshScope
+@RefreshScope//暴露refersh方法
 public class TestController {
 
     @Value("${redis.username}")
