ef210c6e51aa64bfe110abb11a993ce32a345e9c
ex-heguitang <ex-heguitang@banketech.com>
2019-02-18 15:31:01 +0800

    字节码常量池分析

---
+++ javaBasics/src/main/java/jvm/classFile/Test.java
@@ -0,0 +1,19 @@
+package jvm.classFile;
+
+/**
+ * @Author heguitang
+ * @Date 2019/2/18 10:20
+ * @Version 1.0
+ * @Desc 字节码常量池分析
+ */
+public class Test {
+
+    private String attr = "strAttribute";
+    private static String static_attr = "staticAttribute";
+    private final String final_attr = "finalAttribute";
+    private final static String final_static_attr = "finalStaticAttribute";
+
+    private final Integer finaInt = 12345678;
+    private Integer number = 87654321;
+
+}
