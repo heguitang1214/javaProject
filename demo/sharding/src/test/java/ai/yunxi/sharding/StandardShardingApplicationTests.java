8fd2d042aee5d2691c0cb6c76f8e34ea744da6c4
ex-heguitang <ex-heguitang@banketech.com>
2019-02-28 14:32:07 +0800

    sharding-JDBC：测试广播表和行表达式分表分库

--- demo/sharding/src/test/java/ai/yunxi/sharding/StandardShardingApplicationTests.java
+++ demo/sharding/src/test/java/ai/yunxi/sharding/StandardShardingApplicationTests.java
@@ -2,29 +2,59 @@ package ai.yunxi.sharding;
 
 import ai.yunxi.sharding.model.*;
 import ai.yunxi.sharding.service.OrderService;
+import ai.yunxi.sharding.service.ProvinceService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
+/**
+ * 1.测试广播表:插入到所有的表中
+ * 2.测试使用表达式进行数据分片
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ShardingApplication.class)
 public class StandardShardingApplicationTests {
 
     @Autowired
+    private ProvinceService provinceService;
+    @Autowired
     OrderService orderService;
 
+    /**
+     * 测试广播表
+     */
+    @Test
+    public void test() {
+        Province pro = new Province();
+        pro.setId(200);
+        pro.setName("上海");
+        provinceService.save(pro);
+    }
+
+
+    /**
+     * 测试使用表达式进行数据分片
+     */
     @Test
     public void contextLoads() {
-        Order order = OrderGenerator.generate();
-        order.setUserId(10000000);
-        order.setOrderId(1000000);
-        OrderItem orderItem = ItemGenerator.generate();
-        orderItem.setUserId(order.getUserId());
-        orderItem.setOrderId(order.getOrderId());
-        orderService.save(order, orderItem);
+        orderService.save();
     }
+
+
+
+//    @Test
+//    public void contextLoads() {
+//        Order order = OrderGenerator.generate();
+//        order.setUserId(10000000);
+//        order.setOrderId(1000000);
+//        OrderItem orderItem = ItemGenerator.generate();
+//        orderItem.setUserId(order.getUserId());
+//        orderItem.setOrderId(order.getOrderId());
+//        orderService.save(order, orderItem);
+//    }
 
 }
 
