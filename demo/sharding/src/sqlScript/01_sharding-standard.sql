dcbf7aa7921636c77141b34154032546498ebc09
ex-heguitang <ex-heguitang@banketech.com>
2019-03-01 14:22:52 +0800

    sharding-jdbc SQL脚本

--- demo/sharding/src/sqlScript/01_sharding-standard.sql
+++ demo/sharding/src/sqlScript/01_sharding-standard.sql
@@ -1,5 +1,135 @@
+create database ds0;
+create database ds1;
+
+use ds0;
 CREATE TABLE `t_province` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) CHARACTER SET utf8 DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(64) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=201
+;
+
+CREATE TABLE `t_order0` (
+	`id` INT(64) NOT NULL AUTO_INCREMENT,
+	`user_id` INT(64) NULL DEFAULT NULL,
+	`order_id` INT(64) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `t_order1` (
+	`id` INT(64) NOT NULL AUTO_INCREMENT,
+	`user_id` INT(64) NULL DEFAULT NULL,
+	`order_id` INT(64) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `t_order_item0` (
+	`order_id` INT(64) NOT NULL,
+	`user_id` INT(64) NOT NULL,
+	`brand_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`product_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`order_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`pay_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`total_price` FLOAT NOT NULL,
+	`discount` INT(64) NOT NULL,
+	`pay_price` FLOAT NOT NULL,
+	PRIMARY KEY (`order_id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `t_order_item1` (
+	`order_id` INT(64) NOT NULL,
+	`user_id` INT(64) NOT NULL,
+	`brand_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`product_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`order_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`pay_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`total_price` FLOAT NOT NULL,
+	`discount` INT(64) NOT NULL,
+	`pay_price` FLOAT NOT NULL,
+	PRIMARY KEY (`order_id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+
+
+
+
+
+use ds1;
+CREATE TABLE `t_province` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(64) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=201
+;
+
+
+CREATE TABLE `t_order0` (
+	`id` INT(64) NOT NULL AUTO_INCREMENT,
+	`user_id` INT(64) NULL DEFAULT NULL,
+	`order_id` INT(64) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `t_order1` (
+	`id` INT(64) NOT NULL AUTO_INCREMENT,
+	`user_id` INT(64) NULL DEFAULT NULL,
+	`order_id` INT(64) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `t_order_item0` (
+	`order_id` INT(64) NOT NULL,
+	`user_id` INT(64) NOT NULL,
+	`brand_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`product_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`order_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`pay_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`total_price` FLOAT NOT NULL,
+	`discount` INT(64) NOT NULL,
+	`pay_price` FLOAT NOT NULL,
+	PRIMARY KEY (`order_id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `t_order_item1` (
+	`order_id` INT(64) NOT NULL,
+	`user_id` INT(64) NOT NULL,
+	`brand_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`product_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`order_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`pay_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`total_price` FLOAT NOT NULL,
+	`discount` INT(64) NOT NULL,
+	`pay_price` FLOAT NOT NULL,
+	PRIMARY KEY (`order_id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
--- demo/sharding/src/sqlScript/03_sharding-complex.sql
+++ demo/sharding/src/sqlScript/03_sharding-complex.sql
@@ -1,5 +1,212 @@
+use ds0;
+CREATE TABLE `t_order0_0` (
+	`id` INT(64) NOT NULL AUTO_INCREMENT,
+	`user_id` INT(64) NULL DEFAULT NULL,
+	`order_id` INT(64) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `t_order0_1` (
+	`id` INT(64) NOT NULL AUTO_INCREMENT,
+	`user_id` INT(64) NULL DEFAULT NULL,
+	`order_id` INT(64) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `t_order1_0` (
+	`id` INT(64) NOT NULL AUTO_INCREMENT,
+	`user_id` INT(64) NULL DEFAULT NULL,
+	`order_id` INT(64) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `t_order1_1` (
+	`id` INT(64) NOT NULL AUTO_INCREMENT,
+	`user_id` INT(64) NULL DEFAULT NULL,
+	`order_id` INT(64) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `t_order_item0_0` (
+	`order_id` INT(64) NOT NULL,
+	`user_id` INT(64) NOT NULL,
+	`brand_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`product_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`order_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`pay_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`total_price` FLOAT NOT NULL,
+	`discount` INT(64) NOT NULL,
+	`pay_price` FLOAT NOT NULL,
+	PRIMARY KEY (`order_id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `t_order_item0_1` (
+	`order_id` INT(64) NOT NULL,
+	`user_id` INT(64) NOT NULL,
+	`brand_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`product_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`order_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`pay_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`total_price` FLOAT NOT NULL,
+	`discount` INT(64) NOT NULL,
+	`pay_price` FLOAT NOT NULL,
+	PRIMARY KEY (`order_id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `t_order_item1_0` (
+	`order_id` INT(64) NOT NULL,
+	`user_id` INT(64) NOT NULL,
+	`brand_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`product_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`order_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`pay_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`total_price` FLOAT NOT NULL,
+	`discount` INT(64) NOT NULL,
+	`pay_price` FLOAT NOT NULL,
+	PRIMARY KEY (`order_id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `t_order_item1_1` (
+	`order_id` INT(64) NOT NULL,
+	`user_id` INT(64) NOT NULL,
+	`brand_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`product_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`order_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`pay_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`total_price` FLOAT NOT NULL,
+	`discount` INT(64) NOT NULL,
+	`pay_price` FLOAT NOT NULL,
+	PRIMARY KEY (`order_id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
 
 
 
 
+use ds1;
+CREATE TABLE `t_order0_0` (
+	`id` INT(64) NOT NULL AUTO_INCREMENT,
+	`user_id` INT(64) NULL DEFAULT NULL,
+	`order_id` INT(64) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
 
+CREATE TABLE `t_order0_1` (
+	`id` INT(64) NOT NULL AUTO_INCREMENT,
+	`user_id` INT(64) NULL DEFAULT NULL,
+	`order_id` INT(64) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `t_order1_0` (
+	`id` INT(64) NOT NULL AUTO_INCREMENT,
+	`user_id` INT(64) NULL DEFAULT NULL,
+	`order_id` INT(64) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `t_order1_1` (
+	`id` INT(64) NOT NULL AUTO_INCREMENT,
+	`user_id` INT(64) NULL DEFAULT NULL,
+	`order_id` INT(64) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `t_order_item0_0` (
+	`order_id` INT(64) NOT NULL,
+	`user_id` INT(64) NOT NULL,
+	`brand_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`product_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`order_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`pay_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`total_price` FLOAT NOT NULL,
+	`discount` INT(64) NOT NULL,
+	`pay_price` FLOAT NOT NULL,
+	PRIMARY KEY (`order_id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `t_order_item0_1` (
+	`order_id` INT(64) NOT NULL,
+	`user_id` INT(64) NOT NULL,
+	`brand_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`product_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`order_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`pay_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`total_price` FLOAT NOT NULL,
+	`discount` INT(64) NOT NULL,
+	`pay_price` FLOAT NOT NULL,
+	PRIMARY KEY (`order_id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `t_order_item1_0` (
+	`order_id` INT(64) NOT NULL,
+	`user_id` INT(64) NOT NULL,
+	`brand_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`product_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`order_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`pay_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`total_price` FLOAT NOT NULL,
+	`discount` INT(64) NOT NULL,
+	`pay_price` FLOAT NOT NULL,
+	PRIMARY KEY (`order_id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `t_order_item1_1` (
+	`order_id` INT(64) NOT NULL,
+	`user_id` INT(64) NOT NULL,
+	`brand_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`product_name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`order_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`pay_date` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`total_price` FLOAT NOT NULL,
+	`discount` INT(64) NOT NULL,
+	`pay_price` FLOAT NOT NULL,
+	PRIMARY KEY (`order_id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
