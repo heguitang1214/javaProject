093cace000ac32f8b072253132d8d1ae57faad5f
Tang <he_guitang@163.com>
2019-04-15 12:45:03 +0800

    工具类：集合判断

--- javaBasics/src/main/java/utils/CollectionUtils.java
+++ javaBasics/src/main/java/utils/CollectionUtils.java
@@ -4,59 +4,45 @@ import java.util.Collection;
 import java.util.Map;
 
 /**
- * @author carter
- * create time    2017-10-10
+ * 集合判断工具类
  */
 public final class CollectionUtils {
 
-
     /**
      * 判断Map是否为空或者空集合
-     * @param map
+     *
+     * @param map map
      * @return 为null或者为空集合返回true
      */
-    public static boolean isNullOrEmpty(Map map)
-    {
-        if(null == map) return true;
-        if(map.isEmpty()) return true;
-        return false;
+    public static boolean isNullOrEmpty(Map map) {
+        return null == map || map.isEmpty();
     }
 
     /**
      * 判断集合是空或者NULL
-     * @param collection
-     * @return
      */
-    public static boolean isNullOrEmpty(Collection collection)
-    {
-        if(null == collection) return true;
-        if(collection.isEmpty()) return true;
-        return false;
+    public static boolean isNullOrEmpty(Collection collection) {
+        return null == collection || collection.isEmpty();
     }
 
 
     /**
      * 判断集合不是NULL也不是Empty
-     * @param collection
-     * @return
      */
-    public static boolean isNotNullAndEmpty(Collection collection)
-    {
+    public static boolean isNotNullAndEmpty(Collection collection) {
         return !isNullOrEmpty(collection);
     }
-    
+
     public static boolean isNotNullAndEmpty(Map map) {
         return !isNullOrEmpty(map);
     }
 
 
-    public static <K,V>  boolean isNotEmpty(Map<K, V> map) {
-        if(map==null) return false;
-        if(map.isEmpty()) return false;
-        return true;
+    public static <K, V> boolean isNotEmpty(Map<K, V> map) {
+        return !(map == null || map.isEmpty());
     }
-    
-    public static <K,V>  boolean isEmpty(Map<K, V> map) {
+
+    public static <K, V> boolean isEmpty(Map<K, V> map) {
         return !isNotEmpty(map);
     }
 }
