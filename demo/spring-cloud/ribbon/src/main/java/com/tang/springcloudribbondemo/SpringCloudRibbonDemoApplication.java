056acbb61ebe797593dc4f021b815c45200e7796
he_guitang@163.com <hgt11391214abc>
2019-05-03 15:46:01 +0800

    SpringCloud：ribbon

--- demo/spring-cloud/ribbon/src/main/java/com/tang/springcloudribbondemo/SpringCloudRibbonDemoApplication.java
+++ demo/spring-cloud/ribbon/src/main/java/com/tang/springcloudribbondemo/SpringCloudRibbonDemoApplication.java
@@ -1,14 +1,12 @@
 package com.tang.springcloudribbondemo;
 
-import java.util.concurrent.TimeUnit;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-import okhttp3.OkHttpClient;
+//import okhttp3.OkHttpClient;
 
 @SpringBootApplication
 public class SpringCloudRibbonDemoApplication {
@@ -17,29 +15,29 @@ public class SpringCloudRibbonDemoApplication {
 		SpringApplication.run(SpringCloudRibbonDemoApplication.class, args);
 	}
 
-//	@Bean
-//	@LoadBalanced
-//	public RestTemplate restTemplate(){
-//		return new RestTemplate();
-//	}
-
 	@Bean
 	@LoadBalanced
 	public RestTemplate restTemplate(){
-		return new RestTemplate(new OkHttp3ClientHttpRequestFactory(okHttpClient()));
+		return new RestTemplate();
 	}
 
-	/**
-     * http client
-     * @return okHttp
-     */
-    @Bean
-    public OkHttpClient okHttpClient() {
-        OkHttpClient.Builder builder = new OkHttpClient.Builder();
-        builder.connectTimeout(30, TimeUnit.SECONDS)
-                .readTimeout(10, TimeUnit.SECONDS)
-                .writeTimeout(10,TimeUnit.SECONDS)
-                .retryOnConnectionFailure(true);
-        return builder.build();
-    }
+//	@Bean
+//	@LoadBalanced
+//	public RestTemplate restTemplate(){
+//		return new RestTemplate(new OkHttp3ClientHttpRequestFactory(okHttpClient()));
+//	}
+
+//	/**
+//     * http client
+//     * @return okHttp
+//     */
+//    @Bean
+//    public OkHttpClient okHttpClient() {
+//        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+//        builder.connectTimeout(30, TimeUnit.SECONDS)
+//                .readTimeout(10, TimeUnit.SECONDS)
+//                .writeTimeout(10,TimeUnit.SECONDS)
+//                .retryOnConnectionFailure(true);
+//        return builder.build();
+//    }
 }
