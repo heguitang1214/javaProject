43030c7de943cbc3420dedd6f8c03c081c58ad09
Tang <he_guitang@163.com>
2019-08-17 14:37:01 +0800

    数据结构与算法：双向链表

---
+++ javaBasics/src/main/java/algorithm/linkedlist/doublelinked/DNode.java
@@ -0,0 +1,38 @@
+package algorithm.linkedlist.doublelinked;
+
+/**
+ * 双链表结点
+ */
+public class DNode<T> {
+
+    /**
+     * 链表数据
+     */
+    public T data;
+
+    /**
+     * 前继指针和后继指针
+     */
+    public DNode<T> prev, next;
+
+    public DNode(T data, DNode<T> prev, DNode<T> next) {
+        this.data = data;
+        this.prev = prev;
+        this.next = next;
+    }
+
+    public DNode(T data) {
+        this(data, null, null);
+    }
+
+    public DNode() {
+        this(null, null, null);
+    }
+
+    @Override
+    public String toString() {
+        return this.data.toString();
+    }
+
+
+}
