04d223a602ac3f852387d14aba37c40320113833
he_guitang@163.com <hgt11391214abc>
2019-09-05 23:04:35 +0800

    数据结构与算法：冒泡排序优化-鸡尾酒排序

--- javaBasics/src/main/java/algorithm/sort/n2/BubbleSort.java
+++ javaBasics/src/main/java/algorithm/sort/n2/BubbleSort.java
@@ -73,6 +73,44 @@ public class BubbleSort {
         }
     }
 
+    // 鸡尾酒排序
+    private static void sort(int[] array) {
+        int tmp = 0;
+        for (int i = 0; i < array.length / 2; i++) {
+            // 有序标记，每一轮的初始值都是true
+            boolean isSorted = true;
+            // 奇数轮，从左向右比较和交换
+            for (int j = i; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                    // 有元素交换，所以不是有序的，标记变为false
+                    isSorted = false;
+                }
+            }
+            if (isSorted) {
+                break;
+            }
+            // 在偶数轮之前，将isSorted重新标记为false
+            isSorted = true;
+
+            for (int j = array.length - i - 1; j > i; j--) {
+                if (array[j] < array[j - 1]) {
+                    tmp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = tmp;
+                    // 因为有元素交换，所以不是有序的，标记变为false
+                    isSorted = false;
+                }
+            }
+            if (isSorted) {
+                break;
+            }
+        }
+    }
+
+
     /**
      * 两个数进行数据交换
      *
@@ -114,6 +152,11 @@ public class BubbleSort {
         int[] arr1 = new int[]{3, 4, 2, 1, 5, 6, 7, 8};
         bubbleSort1(arr1);
         System.out.println(Arrays.toString(arr1));
+
+
+        int[] arr2 = new int[]{2, 3, 4, 5, 6, 7, 8, 1};
+        sort(arr2);
+        System.out.println(Arrays.toString(arr2));
 
         int a = 1, b = 2, aa = 3, bb = 4;
         dataExchange(a, b);
